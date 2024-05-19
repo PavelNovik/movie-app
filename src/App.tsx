7af9024f655a2c5f9c9be5c0d5323162591ce5e6
Pavel Novik <brn2befree@gmail.com>
2024-05-19 19:38:02 +0300

    add some template to check typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import s from './App.module.scss'
 export function App() {
-  return <div>Hello</div>
+  return (
+    <>
+      <div className={s.main}>Movies</div>
+      <div className={s.title}>ArrowFlicks</div>
+      <div className={s.def}>Hello</div>
+    </>
+  )
 }
