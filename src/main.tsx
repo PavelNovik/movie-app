03add9f81741a57b5c6c4e0ad33b2294dec06090
Pavel Novik <brn2befree@gmail.com>
2024-05-28 21:02:30 +0300

    add some changes in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { MantineProvider } from '@mantine/core'
 import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
@@ -8,6 +9,8 @@ import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
