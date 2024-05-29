5eca6880167ae6cec08e4b3568f68e3534aab77b
Pavel Novik <brn2befree@gmail.com>
2024-05-29 17:03:46 +0300

    change logic and add styles

--- src/pages/movies-page.tsx
+++ src/pages/movies-page.tsx
@@ -1,12 +1,17 @@
+import { ArrowDown } from '@/assets/icons/arrowDown'
+import { Find } from '@/assets/icons/find'
 import { MovieCard } from '@/components/ui/card/movie-card'
 import { Conatiner } from '@/components/ui/container/conatiner'
 import { useGetConfigurationQuery } from '@/services/configuration/configuration-api'
 import { useGetMovieGenresQuery } from '@/services/genres/genre-api'
 import { GenreType } from '@/services/genres/genres-types'
 import { useGetPopularMoviesListQuery } from '@/services/movie-list/movie-list-api'
+import { Button, Loader, Select, Text } from '@mantine/core'
+
+import s from './movies-page.module.scss'
 
 export const MoviesPage = () => {
-  const { data } = useGetPopularMoviesListQuery()
+  const { data, isLoading } = useGetPopularMoviesListQuery()
   const { data: configuration } = useGetConfigurationQuery()
   const { data: genres } = useGetMovieGenresQuery()
 
@@ -16,16 +21,72 @@ export const MoviesPage = () => {
   console.log(genres)
 
   return (
-    <Conatiner>
-      {data?.results.map(movie => (
-        <MovieCard
-          baseUrl={configuration?.images.base_url as string}
-          genres={genres?.genres as GenreType[]}
-          key={movie.id}
-          movie={movie}
-          size={configuration?.images.poster_sizes[0] as string}
+    <div>
+      {isLoading && <Loader />}
+      <h2>Movies</h2>
+      <Conatiner>
+        <Select
+          checkIconPosition={'right'}
+          className={s.filter__select}
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+          label={'Genres'}
+          placeholder={'Select genre'}
+          rightSection={<ArrowDown />}
+        />{' '}
+        <Select
+          checkIconPosition={'right'}
+          className={s.filter__select}
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+          label={'Release year'}
+          placeholder={'Select release year'}
+          rightSection={<ArrowDown />}
         />
-      ))}
-    </Conatiner>
+        <Select
+          checkIconPosition={'right'}
+          className={s.filter__select_rating}
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+          label={'Ratings'}
+          placeholder={'From'}
+        />
+        <Select
+          checkIconPosition={'right'}
+          className={s.filter__select_rating}
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+          label={' '}
+          placeholder={'To'}
+        />
+        <Button color={'gray'} variant={'transparent'}>
+          Reset filters
+        </Button>
+      </Conatiner>
+      <Conatiner>
+        <Select
+          checkIconPosition={'right'}
+          className={s.filter__select}
+          data={['React', 'Angular', 'Vue', 'Svelte']}
+          label={'Sort by'}
+          placeholder={'Select sorting'}
+          rightSection={<ArrowDown />}
+        />
+      </Conatiner>
+      <Conatiner>
+        {data &&
+          data.results.map(movie => (
+            <MovieCard
+              baseUrl={configuration?.images.base_url as string}
+              genres={genres?.genres as GenreType[]}
+              key={movie.id}
+              movie={movie}
+              size={configuration?.images.poster_sizes[0] as string}
+            />
+          ))}
+        {!isLoading && !data && (
+          <Conatiner>
+            <Find />
+            <Text>We don&apos;t have such movies, look for another one</Text>
+          </Conatiner>
+        )}
+      </Conatiner>
+    </div>
   )
 }
