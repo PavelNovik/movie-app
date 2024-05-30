3d9b05b2930bb20cdf38098c6571a9da979c2651
Pavel Novik <brn2befree@gmail.com>
2024-05-31 02:48:31 +0300

    add class names in props

--- src/components/ui/container/conatiner.tsx
+++ src/components/ui/container/conatiner.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react'
 
+import clsx from 'clsx'
+
 import s from './container.module.scss'
 
 type ContainerProps = {
   children: ReactNode
+  className?: string
 }
-export const Conatiner = ({ children }: ContainerProps) => {
-  return <div className={s.container}>{children}</div>
+export const Conatiner = ({ children, className }: ContainerProps) => {
+  return <div className={clsx(s.container, className)}>{children}</div>
 }
